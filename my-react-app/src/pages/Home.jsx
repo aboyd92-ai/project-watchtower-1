adc4acea263e3473f340169ae4310e95f864a6dd
AaronB <to.aboyd@gmail.com>
2025-11-12 20:39:52 -0600

    Added HTML Code to Home Page

---
+++ my-react-app/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <section>
+            <h1>Welcome to Watchtower</h1>
+            <p>Your trusted platform for reporting and tracking safety incidents in your community.</p>
+        </section>
+    )
+}
