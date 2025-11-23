7e6543d172c5576162c79f062860deb2dbf36869
AaronB <to.aboyd@gmail.com>
2025-11-22 20:29:39 -0600

    Imported and incorporated The Arch image code here along along with using the "Alt Text" on image.

--- my-react-app/src/pages/About.jsx
+++ my-react-app/src/pages/About.jsx
@@ -1,7 +1,17 @@
+import aboutimage from '../pages/stl-arch.jpg';
+
 export default function About() {
     return (
-        <section>
+        <section className="about-page">
             <h1>About Watchtower</h1>
+
+            <div className="about-image-wrapper">
+            <img
+            src={aboutimage}
+            alt="St. Louis Architecture"
+            className="about-image"
+            />
+            </div>
             <p>Watchtower is a community-driven platform dedicated to enhancing public safety by enabling users to report and track safety incidents and stay informed.
                 it's mission is to empower individuals with the tools and information they need to make safer choices in their daily lives. along with reporting incidents in real time so everyone can stay informed and vigilant.
             </p>
